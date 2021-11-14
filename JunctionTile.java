21a978080761e0bffab94ac29e5a3c945d8e5567
deShortOne <jingshianggu@gmail.com>
2021-11-14 12:26:47 +0000

    Renamed Bridge.java to Junction.java

---
+++ JunctionTile.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+/**
+ * 
+ * @author 2010573
+ *
+ */
+public class JunctionTile extends Tile {
+	
+	private Random rand = new Random();
+
+	/**
+	 * Will choose a random direction for each Rat to go to.
+	 */
+	public void getNextDirection() {
+		// TODO Auto-generated method stub
+	}
+
+}
