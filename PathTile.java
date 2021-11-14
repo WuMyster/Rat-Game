357167220fb2a1e0b760890dc2f4f6b57ae9eb10
deShortOne <jingshianggu@gmail.com>
2021-11-14 11:02:42 +0000

    PathTile has working getDirection but not tested
    PathTile will run automatically and tell each Rat Class which direction to go. Will need method from Rat Class to tell it direction to go to

--- PathTile.java
+++ PathTile.java
@@ -1,43 +1,72 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * 
+ * @author 2010573
+ *
+ */
 public class PathTile extends Tile {
+
+	private Direction[] directions = new Direction[2]; //Directions to other tiles
 	
-	private HashMap<Tile, Direction> neighbourTiles = new HashMap<>(2);
-	
-	private Item itemOnTile;
-	private int itemHP = 0;
-	private Boolean isBlocked = false;
-	
-	private HashMap<Direction, ArrayList<Rat>> nextBlock = new HashMap<>();
-	private HashMap<Direction, ArrayList<Rat>> currBlock = new HashMap<>();
-	
-	
-	public void setNeighbourTiles (Tile[] tiles, Direction[] direction) {
-		for (int i = 0; i < 2; i++) {
-			neighbourTiles.put(tiles[i], direction[i]);
+	/**
+	 * Will go through list of rats on tile and tell the rat class where to go and tile class which rats are going to it and from what direction
+	 */
+	public void getNextDirection() {
+		for (Direction prevDirection : currBlock.keySet()) {			
+			ArrayList<Rat> ratList = currBlock.get(prevDirection);
+			if (!ratList.isEmpty()) {
+
+				Direction goTo;
+				if (directions[0] == prevDirection) {
+					goTo = directions[1];
+				} else {
+					goTo = directions[0];
+				}
+		
+				//Number of rats that can keep going onwards
+				int ratsGetThrough;
+				if (neighbourTiles.get(goTo).isTileBlocked()) {
+					ratsGetThrough = damageStopSign(currBlock.get(prevDirection).size());
+				} else {
+					ratsGetThrough = currBlock.get(prevDirection).size();
+				}
+				
+				int i;
+				for (i = 0; i < ratsGetThrough; i++) {
+					//Tell rat to go to previous direction
+					//Tell that Tile the direction the rat came from using prevDirection.opposite()
+				}
+				for (; i < ratList.size(); i++) {
+					//Tell rat to go to previous direction
+					//Tell that Tile the direction the rat came from using goTo.opposite()
+				}
+				
+			}
 		}
+		currBlock = nextBlock;
+		nextBlock = new HashMap<>();
 	}
 	
-	public Direction getNextDirection() {
-		return Direction.NORTH;
-	}
-	
-	public void placeStopSign() {
-		itemHP = 20;
-		isBlocked = true;
-	}
-	
-	public Boolean isTileBlocked() {
-		return isBlocked;
-	}
-	
-	public int itemHealth(int n) {
-		itemHP -= n;
-		if (itemHP >= 0) {
-			return 0;
+	/**
+	 * Add rat from other tile to this tile
+	 * @param r
+	 * @param d
+	 */
+	public void addRat(Rat r, Direction d) {
+		nextBlock.putIfAbsent(d, new ArrayList<Rat>());
+		nextBlock.get(d).add(r);
+		
+		/* ArrayList<Rat> r (Adding a whole list of rats instead of one by one)
+		nextBlock.putIfAbsent(d, new ArrayList<Rat>());
+		if (nextBlock.get(d).isEmpty()) {
+			nextBlock.put(d, r);
+		} else {
+			nextBlock.get(d).addAll(r);
 		}
-		isBlocked = false;
-		return Math.abs(itemHP);
+		*/
 	}
+	
+	
 }
--- Tile.java
+++ Tile.java
@@ -1,4 +1,52 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Tile {
+public abstract class Tile {
 	
+	protected int itemHP = 0;
+	protected int limit;
+	protected Boolean isBlocked = false;
+	
+	protected Item itemOnTile;
+	
+	protected HashMap<Direction, Tile> neighbourTiles = new HashMap<>(limit);
+	
+	//Direction is direction to the previous tile
+	protected HashMap<Direction, ArrayList<Rat>> nextBlock = new HashMap<>();
+	protected HashMap<Direction, ArrayList<Rat>> currBlock = new HashMap<>();
+	
+	public void setNeighbourTiles (Tile[] tiles, Direction[] direction) {
+		for (int i = 0; i < limit; i++) {
+			neighbourTiles.put(direction[i], tiles[i]);
+		}
+	}
+	
+	public abstract void getNextDirection();
+	
+	public Boolean isTileBlocked() {
+		return isBlocked;
+	}
+	
+	/**
+	 * Damages stop sign
+	 * @param n
+	 * @return
+	 */
+	public int damageStopSign(int n) {
+		itemHP -= n;
+		if (itemHP >= 0) {
+			return 0;
+		}
+		isBlocked = false;
+		return Math.abs(itemHP);
+	}
+	
+	/**
+	 * Add stop sign to tile
+	 */
+	public void placeStopSign() {
+		itemHP = 20; //Should call Item class go get health of stop sign
+		isBlocked = true;
+		//GUI
+	}
 }
