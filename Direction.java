7a298a0a9bcd2eea5ac2174a552a14fac393c5bd
deShortOne <jingshianggu@gmail.com>
2021-11-14 11:00:23 +0000

    Added opposite function

--- Direction.java
+++ Direction.java
@@ -1,4 +1,26 @@
 
+/**
+ * Direction which rat can travel??
+ * @author 2010573
+ *
+ */
 public enum Direction {
-	NORTH, EAST, SOUTH, WEST
+	NORTH(1), 
+	SOUTH(0), 
+	EAST(3), 
+	WEST(2);
+	
+	private final int oppo;
+
+	Direction(int oppo) {
+		this.oppo = oppo;
+	}
+	
+	/**
+	 * Returns the opposite direction of current direction
+	 * @return
+	 */
+	Direction opposite() {
+		return Direction.values() [this.oppo];
+	}
 }
