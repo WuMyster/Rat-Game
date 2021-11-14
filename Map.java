1574d1fd232dca92d72d5871a4f0a00f4fd0ac73
deShortOne <jingshianggu@gmail.com>
2021-11-14 09:26:14 +0000

    Added Javadoc and create graph

--- Map.java
+++ Map.java
@@ -7,7 +7,7 @@
 public class Map {
 	
 	private String mapDesign;
-	private int xHeight; //Can reimplement so these aren't stored
+	private int xHeight; //Can re-implement so these aren't stored
 	private int yHeight;
 	private Tile[][] board;
 	
@@ -16,6 +16,12 @@ public class Map {
 	private final static char PATH_TILE = 'P';
 	private final static char BRIDGE_TILE = 'B';
 	
+	/**
+	 * Creates Map instance
+	 * @param mapDesign input string of map
+	 * @param xHeight	max height of map
+	 * @param yHeight	max width of map
+	 */
 	public Map (String mapDesign, int xHeight, int yHeight) {
 		this.mapDesign = mapDesign;
 		this.xHeight = xHeight;
@@ -30,6 +36,9 @@ public class Map {
 		return board;
 	}
 	
+	/**
+	 * Creates 2d array of map
+	 */
 	private void createBoard() {
 		int counter = 0;
 		for (int i = 0; i < yHeight; i++) {
@@ -46,8 +55,54 @@ public class Map {
 	
 	
 	//Possible to implement the gui part too!
+	/**
+	 * Creates graph of the map
+	 */
 	private void createGraph() {
-		
+		for (int i = 0; i < yHeight; i++) {
+			for (int j = 0; j < xHeight; j++) {
+				
+				if (board[i][j] != null) {
+					int counter = 0;
+					Tile[] tiles = new Tile[4];
+					Direction[] direction = new Direction[4];
+					
+					//Check North
+					if (i != 0) {
+						if (board[i - 1][j] != null) {
+							tiles[counter] = board[i - 1][j];
+							direction[counter++] = Direction.NORTH;
+						}
+					}
+					
+					//Check East
+					if (j != xHeight - 1) {
+						if (board[i][j + 1] != null) {
+							tiles[counter] = board[i][j + 1];
+							direction[counter++] = Direction.EAST;
+						}
+					}
+					
+					//Check South
+					if (i != yHeight - 1) {
+						if (board[i + 1][j] != null) {
+							tiles[counter] = board[i + 1][j];
+							direction[counter++] = Direction.SOUTH;
+						}
+					}
+					
+					//Check West
+					if (j != 0) {
+						if (board[i][j - 1] != null) {
+							tiles[counter] = board[i][j - 1];
+							direction[counter++] = Direction.WEST;
+						}
+					}
+					board[i][j].setNeighbourTiles(tiles, direction);
+				}
+			}
+		}
+			
 	}
 	
 }
