5117dfe5ce4d1dcc76dc19d22337efa1b5a4240c
O J <93672923+Ollie-Swansea-Uni@users.noreply.github.com>
2021-11-24 16:14:48 +0000

    Created RatController Class, outlining methods

---
+++ RatController.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+/**
+ * 
+ * @author Ollie
+ *
+ */
+public class RatController {
+	private ArrayList<Rat> rats;
+	
+	public void breed(Rat rat1, Rat rat2) {
+		
+	}
+	
+	public void killRat(Rat rat1) {
+		
+	}
+	
+	
+	
+}
