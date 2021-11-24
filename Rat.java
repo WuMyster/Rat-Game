fb8bb15c46a6533bdac0ecfd7b7651d184f5a87c
O J <93672923+Ollie-Swansea-Uni@users.noreply.github.com>
2021-11-24 18:07:41 +0000

    getters and setters for variables in rat class

--- Rat.java
+++ Rat.java
@@ -19,6 +19,7 @@ public class Rat {
 	private int yPosition;
 	private int direction;
 	
+	//Constructs a baby rat
 	public Rat(boolean male, int health, int xPos, int yPos, int direction) {
 		this.isMale = male;
 		this.health = health;
@@ -33,6 +34,93 @@ public class Rat {
 		pointsUponDeath = 30;
 	}
 
+	//constructs a rat from a save file
+	public Rat(int age, boolean male, int xPos, int yPos, int direction, boolean pregnant, int hp, boolean sterile, boolean breeding, boolean deathRat) {
+		this.isMale = male;
+		this.health = hp;
+		this.xPosition = xPos;
+		this.yPosition = yPos;
+		this.direction = direction;
+		isBreeding = breeding;
+		isPregnant = pregnant;
+		isDeathRat = deathRat;
+		isSterile = sterile;
+		this.age = age;
+		pointsUponDeath = 30;
+	}
+	
+	
+	public boolean getIsMale() {
+		return isMale;
+	}
+	public void setIsMale(boolean newIsMale) {
+		isMale = newIsMale;
+	}
+	
+	public void setPregnancy(boolean newPregnancyState) {
+		isPregnant = newPregnancyState;
+	}
+	public boolean getPregnancy() {
+		return isPregnant;
+	}
+	
+	public void becomeDeathRat() {
+		isDeathRat = true;
+	}
+	public boolean getDeathRat() {
+		return isDeathRat;
+	}
+	
+	public void sterilise() {
+		isSterile = true;
+	}
+	public boolean getSterile() {
+		return isSterile;
+	}
+	
+	public void setBreedStatus(boolean breedStatus) {
+		isBreeding = breedStatus;
+	}
+	public boolean getIsBreeding() {
+		return isBreeding;
+	}
+	
+	public void incrementAge() {
+		age++;
+	}	
+	public int getAge() {
+		return age;
+	}
+	
+	public void damageRat(int damage) {
+		health -= damage;
+	}
+	public int getHealth() {
+		return health;
+	}
+	
+	public void changePointsUponDeath(int pointsDifference) {
+		pointsUponDeath += pointsDifference;
+	}
+	public int getPointsUponDeath() {
+		return pointsUponDeath;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
