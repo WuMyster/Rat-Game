fe87a5c9fa523287183662db8f1f55f3ffc4e904
O J <93672923+Ollie-Swansea-Uni@users.noreply.github.com>
2021-11-24 14:57:02 +0000

    Update Rat.java

--- Rat.java
+++ Rat.java
@@ -1,9 +1,23 @@
 
 /**
  * 
- * @author 
+ * @author Ollie
  *
  */
 public class Rat {
-
+	private boolean isMale;
+	private boolean isPregnant;
+	private boolean isDeathRat;
+	private boolean isSterile;
+	
+	private int age;
+	private int health;
+	private int pointsUponDeath;
+	
+	private int xPosition;
+	private int yPosition;
+	private int direction;
+	
+	
+	
 }
