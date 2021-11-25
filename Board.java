7b236e1d522f662f6b29bd7fa661b766c254362d
deShortOne <jingshianggu@gmail.com>
2021-11-25 16:40:49 +0000

    Added Extra padding multiplier

--- Board.java
+++ Board.java
@@ -56,6 +56,8 @@ public class Board {
 	 * Constants of Tile letters from string to Tunnel Tile. XX
 	 */
 	private final static char TUNNEL_TILE = 'T';
+	
+	private final static int EXTRA_PADDING = 2;
 
 	/**
 	 * Constructs a {@code Board} from input string.
@@ -69,7 +71,7 @@ public class Board {
 		this.xHeight = xHeight;
 		this.yHeight = yHeight;
 		try {
-			this.board = new TileType[yHeight][xHeight];
+			this.board = new TileType[yHeight * EXTRA_PADDING][xHeight * EXTRA_PADDING];
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -84,7 +86,7 @@ public class Board {
 	}
 	
 	public boolean addStopSign(int x, int y) {
-		TileType t = board[y][x];
+		TileType t = board[y * EXTRA_PADDING][x * EXTRA_PADDING];
 		if (t == null) {
 			return false;
 		} //else if (board[y][x] instanceof tunnelTile) {
@@ -95,8 +97,8 @@ public class Board {
 	}
 	
 	public boolean addBomb(int x, int y) {
-		int startY = y;
-		int startX = x;
+		int startY = y * EXTRA_PADDING;
+		int startX = x * EXTRA_PADDING;
 		
 		TileType t = board[startY][startX];
 		while (t != null) {
@@ -135,11 +137,11 @@ public class Board {
 		for (TileType[] a : board) {
 			for (TileType b : a) {
 				if (b == null) {
-					gc.drawImage(Output.GRASS_IMAGE, x++ * Output.TILE_SIZE, y * Output.TILE_SIZE, Output.TILE_SIZE,
-							Output.TILE_SIZE);
+					gc.drawImage(Output.GRASS_IMAGE, x++ * Output.TILE_SIZE,
+							y * Output.TILE_SIZE, Output.TILE_SIZE,	Output.TILE_SIZE);
 				} else {
-					gc.drawImage(Output.TILE_IMAGE, x++ * Output.TILE_SIZE, y * Output.TILE_SIZE, Output.TILE_SIZE,
-							Output.TILE_SIZE);
+					gc.drawImage(Output.TILE_IMAGE, x++ * Output.TILE_SIZE,
+							y * Output.TILE_SIZE, Output.TILE_SIZE,	Output.TILE_SIZE);
 				}
 			}
 			x = 0;
@@ -164,28 +166,33 @@ public class Board {
 	 * 
 	 */
 	public void placeRat(Rat rats, Direction dir, int x, int y) {
-		board[x][y].addRat(rats, dir);
+		board[x * EXTRA_PADDING][y * EXTRA_PADDING].addRat(rats, dir);
 	}
-
+	
 	/**
-	 * @param rats list of rats to be added in
-	 * @param dir  direction the rat came from
-	 * @param pos  list of position (x, y) from the top left (Might change this)
+	 * Goes through each Tile and moves the rat to the next tile before getting the tiles new list.
 	 */
-	public void placeRatAA(ArrayList<Rat> rats, ArrayList<Direction> dir, ArrayList<int[]> pos) {
-		for (int i = 0; i < rats.size(); i++) {
-			//int[] currPos = pos.get(i);
-			//board[currPos[0]][currPos[1]].addRat(rats.get(i), dir.get(i));
+	public void runAllTiles() {
+		//Send item to Rat make sure to have boolean to know if it is dead or not
+		
+		//Movement
+		for (TileType t : allTiles) {
+			t.setCurrRat();
+		}
+		for (TileType t : allTiles) {
+			t.getNextDirection();
 		}
 	}
+	
+
 
 	/**
 	 * Creates 2d array of the map.
 	 */
 	private void createBoard() {
 		int counter = 0;
-		for (int i = 0; i < yHeight; i++) {
-			for (int j = 0; j < xHeight; j++) {
+		for (int i = 0; i < yHeight * EXTRA_PADDING; i++) {
+			for (int j = 0; j < xHeight * EXTRA_PADDING; j++) {
 				switch (mapDesign.charAt(counter++)) {
 				case GRASS_TILE -> board[i][j] = null;
 				case PATH_TILE -> board[i][j] = new PathTile(i, j);
@@ -196,31 +203,15 @@ public class Board {
 			}
 		}
 	}
-
-	/**
-	 * Goes through each Tile and moves the rat to the next tile before getting the tiles new list.
-	 */
-	public void runAllTiles() {
-		//Send item to Rat make sure to have boolean to know if it is dead or not
-		
-		
-		
-		//Movement
-		for (TileType t : allTiles) {
-			t.setCurrRat();
-		}
-		for (TileType t : allTiles) {
-			t.getNextDirection();
-		}
-	}
+	
 
 	// Possible to implement the gui part too!
 	/**
 	 * Converts the 2d array of the map into a graph.
 	 */
 	private void createGraph() {
-		for (int i = 0; i < yHeight; i++) {
-			for (int j = 0; j < xHeight; j++) {
+		for (int i = 0; i < yHeight * EXTRA_PADDING; i++) {
+			for (int j = 0; j < xHeight * EXTRA_PADDING; j++) {
 
 				if (board[i][j] != null) {
 					allTiles.add(board[i][j]);
