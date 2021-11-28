8fc54686274cab2220b43ef88c077b402283a5e7
Andrew <andrew.wumyster@googlemail.com>
2021-11-28 17:54:05 +0000

    Update bomb item
    Need to figure out how to traverse path to kill all rats

--- Bomb.java
+++ Bomb.java
@@ -1,7 +1,17 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Bomb {
     int countdown = 5; // 5 for the time being
 
     public void itemAction (PathTile tile) {
-        // Figure out a way to countdown and "detonate", kill rat for entire path
+        Timer timer = new Timer();
+        timer.schedule(new Detonate(), countdown * 1000);
+    }
+
+    class Detonate extends TimerTask {
+        public void run () {
+            // Figure out how to traverse path and kill all rats
+        }
     }
 }
