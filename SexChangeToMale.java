6bc7ec71fe113b480e4e38d53e9157e759fd75b4
Andrew <andrew.wumyster@googlemail.com>
2021-11-28 12:29:18 +0000

    Split SexChange class into two classes as the game has two separate items

---
+++ SexChangeToMale.java
@@ -0,0 +1,10 @@
+public class SexChangeToMale extends Item {
+    public void itemAction(Rat a) {
+        if (a.getIsMale() == false) {
+            a.setIsMale(true);
+        }
+
+        // Need to see how inventory is implemented to implement this properly
+        count -=1;
+    }
+}
