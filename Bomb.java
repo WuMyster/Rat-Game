5b36e02dd58e3200f2822f423c544a06aab0e5bd
Andrew <andrew.wumyster@googlemail.com>
2021-11-29 19:22:07 +0000

    Bomb now detonates after amount of time.

--- Bomb.java
+++ Bomb.java
@@ -2,16 +2,96 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Bomb {
-    int countdown = 5; // 5 for the time being
+    final int COUNTDOWN = 3; // 3 for the time being. Number denotes seconds.
+    final int COUNTDOWN_IN_MS = COUNTDOWN * 1000;
+    int x;
+    int y;
+    Timer timer = new Timer();
 
-    public void itemAction (PathTile tile) {
-        Timer timer = new Timer();
-        timer.schedule(new Detonate(), countdown * 1000);
+    public boolean itemAction(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+        this.timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            public void run() {
+                detonate();
+            }
+        };
+
+        timer.schedule(task, COUNTDOWN_IN_MS);
+
+        return true;
     }
+
+
+    public boolean detonate() {
+        y *= Board.EXTRA_PADDING;
+        x *= Board.EXTRA_PADDING;
+        int startY = y;
+        int startX = x;
+        TileType[][] board = Board.getBoard();
+
+        TileType t = board[startY][startX];
+        while (t != null) {
+            t.blowUp();
+            t = board[y--][x];
+        }
+
+        t = board[startY][startX];
+        y = startY;
+        x = startX;
+        while (t != null) {
+            t.blowUp();
+            t = board[y++][x];
+        }
+
+        t = board[startY][startX];
+        y = startY;
+        x = startX;
+        while (t != null) {
+            t.blowUp();
+            t = board[y][x--];
+        }
+
+        t = board[startY][startX];
+        y = startY;
+        x = startX;
+        while (t != null) {
+            t.blowUp();
+            t = board[y][x++];
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
+    /*
+    public boolean itemAction (int x, int y) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("test");
+            }
+        }, 5 * 1000);
+    }
+
+    public void run() {
+
+    }
+
 
     class Detonate extends TimerTask {
         public void run () {
             // Figure out how to traverse path and kill all rats
         }
     }
+     */
 }
