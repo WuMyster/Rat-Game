a415508ed78b7467214e87ce58aa3e5ec9e75005
deShortOne <jingshianggu@gmail.com>
2021-11-29 15:08:28 +0000

    Set images for each rat

--- RatType.java
+++ RatType.java
@@ -1,19 +1,30 @@
 import javafx.scene.image.Image;
 
 /**
- * 
+ * All types of rats and default values for them.
  * @author Jing Shiang Gu
  *
  */
 public enum RatType {
-	DEATH (1, 2), 
-	MALE (1, 1), 
-	FEMALE(1, 1), 
-	BABY(2, 2);
+	DEATH (1, 2, "DeathRat.png"), 
+	MALE (1, 1, "MaleRat.png"), 
+	FEMALE(1, 1, "FemaleRat.png"), 
+	BABY(2, 2, "BabyRat.png");
 	
+	/**
+	 * Size multiplier of rat. Bigger is smaller
+	 */
 	private final int size;
+	
+	/**
+	 * Speed multiplier of rat. Bigger is faster.
+	 */
 	private final int speed;
-	private Image art;
+	
+	/**
+	 * Image of rat.
+	 */
+	private Image image;
 	
 	/**
 	 * Returns size of rat. Bigger is smaller.
@@ -31,23 +42,22 @@ public enum RatType {
 		return speed;
 	}
 	
+	/**
+	 * Returns image of rat.
+	 * @return image of rat
+	 */
 	public Image getImage() {
-		return art;
-	}
-	
-	public void setImage(Image art) {
-		this.art = art;
-	}
-	
+		return image;
+	}	
 	
 	/**
 	 * Set values of each type of rat.
 	 * @param size how big rat is (bigger is smaller)
 	 * @param speed how fast rat can move (bigger is faster)
 	 */
-	private RatType(int size, int speed) {
+	private RatType(int size, int speed, String imageLoc) {
 		this.size = size;
 		this.speed = speed;
+		this.image = new Image(imageLoc);
 	}
-	
 }
