c3b58a77b85caf0afa4e19691782a7e82dba05d1
deShortOne <jingshianggu@gmail.com>
2021-12-01 14:28:33 +0000

    Added methods to aid movement to output

--- DeathRat.java
+++ DeathRat.java
@@ -3,13 +3,37 @@
 public class DeathRat {
 	
 	Rat rat;
+	int[] xyPos;
+	Direction d;
+	int move;
 	
 	/**
-	 * Constructor for Death Rat
+	 * Constructor for Death Rat with basically garbage args.
 	 * @author j
 	 */
 	public DeathRat() {
 		rat = new Rat(50, true, true, 20, true, true, true);
+	}
+	
+	/**
+	 * Initial movement parameters for rat.
+	 * @param xyPos xy position it started on
+	 * @param d		direction it's heading
+	 * @author J
+	 */
+	public void initalMove(int[] xyPos, Direction d) {
+		this.xyPos = xyPos;
+		this.d = d;
+		this.move = 4; // TODO chang to constant
+	}
+	
+	/**
+	 * Change it's move limit.
+	 * @param move limit it's going forwards to
+	 * @author J
+	 */
+	public void changeMove(int move) {
+		this.move = move;
 	}
 	
 	public Rat itemAction() {
