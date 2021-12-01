b44f063626a7c1fc25992fd5415bdc9b131d1dab
deShortOne <jingshianggu@gmail.com>
2021-12-01 14:10:02 +0000

    Created initial constructor for Death Rat

--- DeathRat.java
+++ DeathRat.java
@@ -1,7 +1,21 @@
+
+
 public class DeathRat {
-    public Rat itemAction() {
-        Rat deathRat = new Rat(true, 10);
-        deathRat.becomeDeathRat();
-        return deathRat;
-    }
+	
+	Rat rat;
+	
+	/**
+	 * Constructor for Death Rat
+	 * @author j
+	 */
+	public DeathRat() {
+		rat = new Rat(50, true, true, 20, true, true, true);
+	}
+	
+	public Rat itemAction() {
+		Rat deathRat = new Rat(true, 10);
+		deathRat.becomeDeathRat();
+		return deathRat;
+	}
+	
 }
