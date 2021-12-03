0db516eb63caf4fd958a15e009b93f038fe34654
O J <93672923+Ollie-Swansea-Uni@users.noreply.github.com>
2021-12-03 00:07:29 +0000

    Update Rat.java

--- Rat.java
+++ Rat.java
@@ -1,50 +1,94 @@
 
 /**
- * 
- * @author Ollie
+ * Models a single rat
+ * @author Ollie Jarrett
  *
  */
 public class Rat {
+	/**
+	 * The rats sex
+	 */
 	private boolean isMale;
+	/**
+	 * Whether the rat is pregnant
+	 */
 	private boolean isPregnant;
+	/**
+	 * Whether the rat is a death rat
+	 */
 	private boolean isDeathRat;
+	/**
+	 * Whether the rat can breed
+	 */
 	private boolean isSterile;
+	/**
+	 * Whether the rat is currently breeding - breeding rats don't move or interact with other rats
+	 */
 	private boolean isBreeding;
 	
+	/**
+	 * The age of the rat in ticks
+	 */
 	private int age;
+	/**
+	 * The amount of health the rat has
+	 */
 	private int health;
+	/**
+	 * The amount of points rewarded when the rat is killed
+	 */
 	private int pointsUponDeath;
 	
 
 	
-	//Constructs a baby rat
-	public Rat(boolean male, int health) {
-		this.isMale = male;
+	/**
+	 * Constructor for new rats - used at the start of the level or for newly born rats
+	 * @param isMale the rats sex
+	 * @param health the rats health
+	 */
+	public Rat(boolean isMale, int health) {
+		this.isMale = isMale;
 		this.health = health;
 		isBreeding = false;
 		isPregnant = false;
 		isDeathRat = false;
 		isSterile = false;
 		age = 0;
-		pointsUponDeath = 30;
+		pointsUponDeath = 10;
 	}
 
-	//constructs a rat from a save file
-	public Rat(int age, boolean male, boolean pregnant, int hp, boolean sterile, boolean breeding, boolean deathRat) {
-		this.isMale = male;
+	/**
+	 * Constructor for existing rats from a save file
+	 * @param age - how old the rat is
+	 * @param isMale - the rats sex - True = male, False = female
+	 * @param isPregnant - whether the rat is currently pregnant
+	 * @param hp - the rats health
+	 * @param isSterile - whether the rat is sterile - True = sterile, False = not sterile
+	 * @param isBreeding - whether the rat is currently bleeding
+	 * @param isDeathRat - whether the rat is a death rat
+	 */
+	public Rat(int age, boolean isMale, boolean isPregnant, int hp, boolean isSterile, boolean isBreeding, boolean isDeathRat) {
+		this.isMale = isMale;
 		this.health = hp;
-		isBreeding = breeding;
-		isPregnant = pregnant;
-		isDeathRat = deathRat;
-		isSterile = sterile;
+		this.isBreeding = isBreeding;
+		this.isPregnant = isPregnant;
+		this.isDeathRat = isDeathRat;
+		this.isSterile = isSterile;
 		this.age = age;
-		pointsUponDeath = 30;
+		pointsUponDeath = calculatePointsUponDeath();
 	}
 	
-	
+	/**
+	 * returns whether the rat is male
+	 * @return 
+	 */
 	public boolean getIsMale() {
 		return isMale;
 	}
+	/**
+	 * Changes the rat sex to a new input sex
+	 * @param newIsMale - whether the rat should be male after the interaction
+	 */
 	public void setIsMale(boolean newIsMale) {
 		isMale = newIsMale;
 	}
@@ -92,22 +136,47 @@ public class Rat {
 		}
 	}
 	
-	
-	public void damageRat(int damage) {
+	/**
+	 * Damages the rat and returns a boolean value for if the rat has died
+	 * @param damage - amount of damage dealt to the rat
+	 * @return a boolean on whether the rat is dead (true = dead)
+	 */
+	public boolean damageRat(int damage) {
 		health -= damage;
+		if(health == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+	/**
+	 * Gets the rats health value
+	 * @return the rats health
+	 */
 	public int getHealth() {
 		return health;
 	}
 	
-	public void changePointsUponDeath(int pointsDifference) {
-		pointsUponDeath += pointsDifference;
-	}
 	public int getPointsUponDeath() {
 		return pointsUponDeath;
 	}
-	
-	
+	/**
+	 * 
+	 * @return
+	 */
+	private int calculatePointsUponDeath() {
+		int points = 10;
+		if(isBreeding) {
+			points += 10;
+		}
+		if(isPregnant) {
+			points += 10;
+		}
+		if(isChild()) {
+			points += 10;
+		}
+		return points;
+	}
 	
 	
 	
