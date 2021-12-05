0b41d81fe1e6fece5995e4a29e8c9dce54c62143
O J <93672923+Ollie-Swansea-Uni@users.noreply.github.com>
2021-12-05 17:38:26 +0000

    Finalised Rat JavaDoc

--- Rat.java
+++ Rat.java
@@ -1,44 +1,23 @@
 /**
- * This class models a single rat
+ * This class models a single rat.
  * @author Ollie Jarrett
- *
+ * @version
  */
 public class Rat {
-	/**
-	 * The rats sex
-	 */
 	private boolean isMale;
-	/**
-	 * Whether the rat is pregnant
-	 */
 	private boolean isPregnant;
-	/**
-	 * Whether the rat is a death rat
-	 */
 	private boolean isDeathRat;
-	/**
-	 * Whether the rat can breed
-	 */
 	private boolean isSterile;
-	/**
-	 * Whether the rat is currently breeding - breeding rats don't move or interact with other rats
-	 */
 	private boolean isBreeding;
-	
-	/**
-	 * The age of the rat in ticks
-	 */
 	private int age;
-	/**
-	 * The amount of health the rat has
-	 */
 	private int health;
 	
 	
 	/**
-	 * Constructor for new rats - used at the start of the level or for newly born rats
-	 * @param isMale the rats sex
-	 * @param health the rats health
+	 * Constructor for new rats.
+	 * Should be used at the start of the level or for newly born rats.
+	 * @param isMale the rats sex.
+	 * @param health the rats health.
 	 */
 	public Rat(boolean isMale) {
 		this.isMale = isMale;
@@ -51,14 +30,15 @@ public class Rat {
 	}
 
 	/**
-	 * Constructor for existing rats from a save file
-	 * @param age - how old the rat is
-	 * @param isMale - the rats sex - True = male, False = female
-	 * @param isPregnant - whether the rat is currently pregnant
-	 * @param hp - the rats health
-	 * @param isSterile - whether the rat is sterile - True = sterile, False = not sterile
-	 * @param isBreeding - whether the rat is currently bleeding
-	 * @param isDeathRat - whether the rat is a death rat
+	 * Constructor for existing rats.
+	 * Used when loading pre-existing rats from a save file.
+	 * @param age - how old the rat is.
+	 * @param isMale - the rats sex - True = male, False = female.
+	 * @param isPregnant - whether the rat is currently pregnant.
+	 * @param hp - the rats health.
+	 * @param isSterile - whether the rat is sterile - True = sterile, False = not sterile.
+	 * @param isBreeding - whether the rat is currently bleeding.
+	 * @param isDeathRat - whether the rat is a death rat.
 	 */
 	public Rat(int age, boolean isMale, boolean isPregnant, int hp, boolean isSterile, boolean isBreeding, boolean isDeathRat) {
 		this.isMale = isMale;
@@ -71,9 +51,9 @@ public class Rat {
 	}
 	
 	/**
-	 * Returns rat type
-	 * @return rat type
-	 * @author J
+	 * Gets the rat type.
+	 * @return rat type.
+	 * @author J.
 	 */
 	public RatType getStatus() {
 		if (isChild()) {
@@ -86,42 +66,49 @@ public class Rat {
 	}
 	
 	/**
-	 * returns whether the rat is male
-	 * @return 
+	 * Gets whether the rat is male.
+	 * @return A boolean value - true = male.
 	 */
 	public boolean getIsMale() {
 		return isMale;
 	}
+	
 	/**
-	 * Changes the rat sex to a new input sex
-	 * @param newIsMale - whether the rat should be male after the interaction
+	 * Sets the rat sex to a new boolean value newIsMale.
+	 * @param newIsMale - whether the rat should be male after the interaction.
 	 */
 	public void setIsMale(boolean newIsMale) {
 		isMale = newIsMale;
 	}
+	
 	/**
-	 * Changes the rat to be pregnant or not pregnant
-	 * @param newPregnancyState - true = pregnant, false = not pregnant
+	 * Set the rat pregnancy to a new boolean value.
+	 * @param newPregnancyState - true = pregnant, false = not pregnant.
 	 */
 	public void setPregnancy(boolean newPregnancyState) {
 		isPregnant = newPregnancyState;
 	}
+	
 	/**
-	 * tells other classes whether the rat is pregnant or not
-	 * @return isPregnant - true = pregnant
+	 * Gets whether the rat is pregnant or not.
+	 * @return isPregnant - true = pregnant.
 	 */
 	public boolean getPregnant() {
 		return isPregnant;
 	}
+	
 	/**
-	 * Tells other classes whether the rat is a death rat
-	 * @return isDeathRat - true = the rat is a death rat
+	 * Gets whether the rat is a death rat.
+	 * @return isDeathRat - true = the rat is a death rat.
 	 */
 	public boolean getDeathRat() {
 		return isDeathRat;
 	}
+	
 	/**
-	 * Sets isSterile to true - the rat can no longer breed
+	 * Sterilises the rat.
+	 * Adult rats will become sterile and can no longer breed.
+	 * Child rats will become sterile adults afterwards.
 	 */
 	public void sterilise() {
 		isSterile = true;
@@ -129,43 +116,49 @@ public class Rat {
 			age = 30;
 		}
 	}
+	
 	/**
-	 * Tells other classes whether the rat is sterile and cannot breed
-	 * @return A boolean for if the rat is sterile
+	 * Gets whether the rat is sterile.
+	 * @return A boolean value - true = sterile.
 	 */
 	public boolean getSterile() {
 		return isSterile;
 	}
+	
 	/**
-	 * Changes whether the rat is currently breeding
-	 * @param breedStatus - true = breeding
+	 * Sets whether the rat is currently breeding.
+	 * @param breedStatus - A boolean value where true = breeding.
 	 */
 	public void setBreedStatus(boolean breedStatus) {
 		isBreeding = breedStatus;
 	}
+	
 	/**
-	 * Tells other classes whether the rat is currently breeding
-	 * @return Boolean value for if the rat is breeding
+	 * Gets whether the rat is currently breeding.
+	 * @return A boolean of whether the rat is breeding.
 	 */
 	public boolean getIsBreeding() {
 		return isBreeding;
 	}
+	
 	/**
-	 * Makes the rat older
+	 * Increments the rat age, making it older.
 	 */
 	public void incrementAge() {
 		age++;
 	}
+	
 	/**
-	 * Tells other classes how old the rat is
-	 * @return An integer containing the rats age
+	 * Gets how old the rat is.
+	 * @return An integer containing the rats age.
 	 */
 	public int getAge() {
 		return age;
 	}
+	
 	/**
-	 * Tells other classes whether the rat is a child
-	 * @return A boolean where true = the rat is a child
+	 * Gets whether the rat is a child.
+	 * @return A boolean where true = the rat is a child.
 	 */
 	public boolean isChild() {
 		if(age<30) {
@@ -177,9 +170,8 @@ public class Rat {
 	}
 	
 	/**
-	 * Damages the rat and returns a boolean value for if the rat has died
-	 * @param damage - amount of damage dealt to the rat
-	 * @return a boolean on whether the rat is dead (true = dead)
+	 * Damages the rat and kills it if its health is depleted.
+	 * @param damage - amount of damage dealt to the rat.
 	 */
 	public void damageRat(int damage) {
 		health -= damage;
@@ -187,23 +179,26 @@ public class Rat {
 			RatController.killRat(this);
 		}
 	}
+	
 	/**
-	 * Gets the rats health value
-	 * @return the rats health
+	 * Gets the rats health value.
+	 * @return the rats health.
 	 */
 	public int getHealth() {
 		return health;
 	}
+	
 	/**
-	 * Tells other classes how many points the rat is worth
-	 * @return An integer containing the point value of the rat
+	 * Gets how many points the rat is worth.
+	 * @return An integer containing the point value of the rat.
 	 */
 	public int getPointsUponDeath() {
 		return calculatePointsUponDeath();
 	}
+	
 	/**
-	 * Calculates how many points the rat is worth
-	 * @return An integer value for the rats worth
+	 * Calculates how many points the rat is worth.
+	 * @return An integer value for the rats worth.
 	 */
 	private int calculatePointsUponDeath() {
 		int points = 10;
@@ -218,8 +213,9 @@ public class Rat {
 		}
 		return points;
 	}
+	
 	/**
-	 * Translates the rat into a string which can be easily stored
+	 * Translates the rat into a string which can be easily stored.
 	 */
 	public String toString() {
 		String output = "";
